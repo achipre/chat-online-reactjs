d5e50298ed55e36d806c7ce4870fd70bbfbf9b28
Alex Chipre <alechipre@gmail.com>
2024-04-06 16:52:29 -0500

    Initialization Project Chat Online

---
+++ .eslintrc.cjs
@@ -0,0 +1,60 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true,
+		"node": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended"
+	],
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"files": [
+				".eslintrc.{js,cjs}"
+			],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module",
+		"ecmaFeactures": { "jsx": true}
+	},
+	"plugins": [
+		"@typescript-eslint",
+		"react",
+		"react-hooks"
+	],
+	"rules": {
+		"react/react-in-jsx-scope": "off", 
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	},
+	"setting": {
+		"react": {
+			"version": "detect"
+		}
+	}
+};
